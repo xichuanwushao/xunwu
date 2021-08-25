49d1b0ea821f02824b274b30b8a76f6c6dc8b3b5
xichuanwushao <avatarwx@163.com>
2021-08-25 14:31:21 +0800

    1-1 创建寻屋网的springboot项目

---
+++ src/main/java/com/xichuan/XunwuProjectApplication.java
@@ -0,0 +1,13 @@
+package com.xichuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XunwuProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XunwuProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xichuan/XunwuProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xichuan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XunwuProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
